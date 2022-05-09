7f4446d8ef6bb27f97636132f77277eda908ed97
Apricity <87al@fresnou.org>
2022-05-09 13:44:05 -0700

    final commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const cardList = document.querySelector('.cardList');
 
+const score = document.querySelector('.score');
+let points = 0;
+
+const mayo = document.querySelector('.mayo');
 
 function addCard(value){
     let card = document.createElement('div');
@@ -23,15 +27,23 @@ cardList.addEventListener('click', function(e){
     if (e.target.classList.contains('active')){
         e.target.classList.remove('active');
         e.target.classList.add('inactive');
+        points++;
+        score.textContent = points;
         return
     }
     e.target.remove();
+    points +=2;
+    score.textContent = points;
     let children = cardList.children;
     if (children.length < 1){
         clearInterval(interval);
     }
 });
 
+mayo.addEventListener('click', function(){
+        points +=5;
+        score.textContent = points;
+});
 buildBoard();
 
 let interval = setInterval(function(){
